070463f88b25a4c13d11c30f34ff66798cd0c555
Iain McDonald <iain.mcdonald@smartsheet.com>
2019-12-01 16:08:20 +0000

    Solution to Day02

---
+++ Day02.Tests/UnitTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+using AdventOfCode2019.Solutions;
+
+namespace AdventOfCode2019.Tests
+{
+    public class TestDay02
+    {
+        [Fact]
+        public void TestSolution1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventOfCode2019.Solutions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var day02 = new Day02();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
